496597be8e361832ae5d6acfa70afd6956087a81
Marcus Põld <marcuspold6@gmail.com>
2021-01-14 15:11:11 +0200

    first commit

---
+++ ChuckForms/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckForms
+{
+    class Joke
+    {
+        public value value { get; set; }
+    }
+    public class value
+    {
+        public string joke { get; set; }
+    }
+}
